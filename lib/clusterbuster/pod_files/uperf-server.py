01ab98c926d4061157007143745ac83df215f520
Robert Krawitz <rkrawitz@redhat.com>
2022-09-27 19:18:46 -0400

    Convert uperf workload to python

--- lib/clusterbuster/pod_files/uperf-server.py
+++ lib/clusterbuster/pod_files/uperf-server.py
@@ -9,14 +9,14 @@ class uperf_server_client(clusterbuster_pod_client):
     Server side of uperf test for clusterbuster
     """
 
-    def __init__(self, **args):
-        super().__init__(args)
+    def __init__(self):
+        super().__init__(initialize_timing_if_needed=False)
         self.listen_port = self._args[0]
 
     def runit(self, process: int):
-        self.timestamp("Starting uperf server on port $listen_port")
+        self.timestamp(f"Starting uperf server on port {self.listen_port}")
         subprocess.run(['uperf', '-s', '-v', '-P', self.listen_port])
         self.timestamp("Done!")
 
 
-uperf_server_client(initialize_timing_if_needed=False).run_workload()
+uperf_server_client().run_workload()
