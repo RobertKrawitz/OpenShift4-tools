3e4fa5b3b94f5286804ca1a81ddbf8afb47fb962
Robert Krawitz <rkrawitz@redhat.com>
2022-09-29 11:52:53 -0400

    Increase the size of the listen queue to avoid requests
    getting dropped; also remove unused modules.

--- lib/clusterbuster/pod_files/drop_cache.pl
+++ lib/clusterbuster/pod_files/drop_cache.pl
@@ -1,19 +1,11 @@
 #!/usr/bin/perl
 
 use Socket;
-use POSIX;
 use strict;
-use Time::Piece;
-use Time::HiRes qw(gettimeofday usleep);
-use Sys::Hostname;
-use File::Basename;
-use JSON;
 $SIG{TERM} = sub { POSIX::_exit(0); };
 require "clientlib.pl";
 
 my ($listen_port) = @ARGV;
-
-my ($pod) = hostname;
 
 timestamp("Cache drop starting");
 my $sockaddr = "S n a4 x8";
@@ -22,7 +14,7 @@ $SIG{TERM} = sub { close SOCK; kill 'KILL', -1; POSIX::_exit(0); };
 setsockopt(SOCK,SOL_SOCKET, SO_REUSEADDR, pack("l",1)) || die "setsockopt reuseaddr: $!\n";
 setsockopt(SOCK,SOL_SOCKET, SO_KEEPALIVE, pack("l",1)) || die "setsockopt keepalive: $!\n";
 bind(SOCK, pack($sockaddr, AF_INET, $listen_port, "\0\0\0\0")) || die "bind: $!\n";
-listen(SOCK, 5) || die "listen: $!";
+listen(SOCK, 128) || die "listen: $!";
 my $mysockaddr = getsockname(SOCK);
 my ($junk, $port, $addr) = unpack($sockaddr, $mysockaddr);
 die "can't get port $port: $!\n" if ($port ne $listen_port);
