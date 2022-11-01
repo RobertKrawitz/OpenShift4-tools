b298980cccbb65dce4b987bebde7bd430b86653b
Robert Krawitz <rkrawitz@redhat.com>
2022-11-01 16:19:02 -0400

    Support cpu, memory, threads, and mutex workloads for sysbench (not database).

--- lib/clusterbuster/reporter/sysbench_reporter.py
+++ lib/clusterbuster/reporter/sysbench_reporter.py
@@ -16,11 +16,20 @@
 
 import re
 from .ClusterBusterReporter import ClusterBusterReporter
+import sys
 
 
 class sysbench_reporter(ClusterBusterReporter):
     def __init__(self, jdata: dict, report_format: str):
         ClusterBusterReporter.__init__(self, jdata, report_format)
+        self._set_header_components(['namespace', 'pod', 'container', 'process_id'])
+        self._is_fileio = 'sysbench_fileio_tests' in jdata['metadata']['options']['workloadOptions']
+        if self._is_fileio:
+            self.__initialize_fileio(jdata)
+        else:
+            self.__initialize_simple(jdata)
+
+    def __initialize_fileio(self, jdata):
         self._sysbench_operations = jdata['metadata']['options']['workloadOptions']['sysbench_fileio_tests']
         self._sysbench_vars_to_copy = ['filesize:precision=3:suffix=B:base=1024',
                                        'blocksize:precision=3:suffix=B:base=1024', 'rdwr_ratio',
@@ -37,9 +46,45 @@ class sysbench_reporter(ClusterBusterReporter):
                 vars_to_copy.append(f'{workload}.{re.sub(r":.*", "", var)}')
         self._add_accumulators(accumulators)
         self._add_fields_to_copy(vars_to_copy)
-        self._set_header_components(['namespace', 'pod', 'container', 'process_id'])
+
+    def __initialize_simple(self, jdata):
+        self._sysbench_operations = jdata['Results']['worker_results'][0]['workloads'].keys()
+        self._sysbench_vars_to_copy = ['threads']
+        accumulators = []
+        vars_to_copy = []
+        for op in self._sysbench_operations:
+            workload = f'workloads.{op}'
+            for var in ['elapsed_time', 'user_cpu_time', 'sys_cpu_time', 'total_events',
+                        'mean_latency_sec', 'max_latency_sec', 'p95_latency_sec']:
+                accumulators.append(f'{workload}.{var}')
+            for var in self._sysbench_vars_to_copy:
+                vars_to_copy.append(f'{workload}.{re.sub(r":.*", "", var)}')
+        self._add_accumulators(accumulators)
+        self._add_fields_to_copy(vars_to_copy)
 
     def __update_report(self, dest: dict, source: dict, sample_row: dict = None):
+        if self._is_fileio:
+            self.__update_report_fileio(dest, source, sample_row)
+        else:
+            self.__update_report_simple(dest, source, sample_row)
+
+    def __update_report_simple(self, dest: dict, source: dict, sample_row: dict = None):
+        if sample_row is None:
+            sample_row = source
+        for op in self._sysbench_operations:
+            pop = f'workload: {op}'
+            dest[pop] = {}
+            dest[pop]['Elapsed Time'] = self._fformat(self._summary['data_run_interval'], 3)
+            dest[pop]['CPU Time'] = self._fformat(source[op]['user_cpu_time'] + source[op]['sys_cpu_time'], 3)
+            for var in ['total_events:precision=3:suffix=events:base=1000:integer=1']:
+                self._copy_formatted_value(var, dest[pop], source[op])
+            for var in self._sysbench_vars_to_copy:
+                self._copy_formatted_value(var, dest[pop], source[op])
+            source[op]['events_rate'] = self._safe_div(source[op]['total_events'], self._summary['data_run_interval'], number_only=True)
+            dest[pop]['events_rate'] = self._prettyprint(self._safe_div(source[op]['total_events'], self._summary['data_run_interval']),
+                                                           precision=3, base=1000, suffix='events/sec')
+
+    def __update_report_fileio(self, dest: dict, source: dict, sample_row: dict = None):
         if sample_row is None:
             sample_row = source
         for op in self._sysbench_operations:
