a9677f25c1cb1154046e89d2d596165bc9cd64d0
Robert Krawitz <rkrawitz@redhat.com>
2024-10-22 10:15:35 -0400

    Add waitforever workload. Purpose is to create a container/VM that allows running something else externally in it.

---
+++ lib/clusterbuster/pod_files/waitforever.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import signal
+from clusterbuster_pod_client import clusterbuster_pod_client
+
+
+class waitforever_client(clusterbuster_pod_client):
+    """
+    Dummy test for clusterbuster; sleep forever.
+    """
+
+    def __init__(self):
+        try:
+            super().__init__()
+        except Exception as err:
+            self._abort(f"Init failed! {err} {' '.join(self._args)}")
+
+    def runit(self, process: int):
+        self._timestamp(f"Starting waitforever")
+        signal.pause()
+
+
+waitforever_client().run_workload()
